c2b723627fb1a258887d051ce277fd2d1f6674e5
尘尘 <78690331+gxy1110@users.noreply.github.com>
2026-06-10 11:59:21 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,41 +3,52 @@ plugins {
     id("kotlin-android")
     id("kotlin-kapt")
 }
+
 android {
-    compileSdk = 34
+    compileSdk = Libs.App.compileSdkVersion
+
+    // 👇 1. 新增：我们的专属钛合金防盗门锁配置
     signingConfigs {
         create("release") {
             storeFile = file("chen.keystore")
             storePassword = "cW8@mK2!pL9#vX5"
             keyAlias = "chen"
             keyPassword = "cW8@mK2!pL9#vX5"
+        }
+    }
+
     defaultConfig {
+        // 👇 2. 恢复为你专属的包名
         applicationId = "com.chenchen.wallpaper"
         minSdk = Libs.App.minSdkVersion
-        targetSdk = 34
+        targetSdk = Libs.App.targetSdkVersion
         versionCode = ReleaseConfig.appVersionCode
         versionName = ReleaseConfig.appVersionName
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+
     buildTypes {
         getByName("release") {
             isMinifyEnabled = true
             isShrinkResources = true
-            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            // 👇 3. 给正式版打上钢印
+            signingConfig = signingConfigs.getByName("release")
         }
         getByName("debug") {
             isMinifyEnabled = false
-            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            // 👇 4. 给测试版也打上钢印
+            signingConfig = signingConfigs.getByName("release")
         }
     }
+
     buildFeatures {
         compose = true
         buildConfig = false
@@ -46,6 +57,7 @@ android {
         resValues = false
         shaders = false
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -53,7 +65,7 @@ android {
 
     packagingOptions {
        resources.excludes.add("/META-INF/AL2.0")
-        resources.excludes.add("/META-INF/LGPL2.1")
+       resources.excludes.add("/META-INF/LGPL2.1")
     }
 
     composeOptions {
@@ -79,6 +91,4 @@ dependencies {
     androidTestImplementation(Libs.AndroidX.Test.Ext.junit)
     
     implementation("com.squareup.okhttp3:okhttp:4.9.3")
-    // 引入 Coil 图片加载库，用于高性能的图库网格显示
-    implementation("io.coil-kt:coil-compose:2.2.2")
 }
